a2b35fed22995e10aa966c8cc9fc0c10412fe3e2
Ariachnide <rb.guerre@gmail.com>
2025-10-28 11:48:19 +0100

    wip docker sequelize config

--- util/database.js
+++ util/database.js
@@ -1,13 +1,7 @@
 import Sequelize from "sequelize";
 
 const sequelize = new Sequelize(
-    process.env.PG_DB,
-    process.env.PG_USER,
-    process.env.PG_PASSWORD,
-    {
-        host: process.env.PG_HOST,
-        dialect: "postgres",
-    }
+    "compass_pg_db://default:defaultPw@compass_pg_db:5432/compass"
 );
 
 export default sequelize;
